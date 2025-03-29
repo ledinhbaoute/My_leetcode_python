6ffdfa0073ce6224e1e68c5892576bb170450f4d
ledinhbaoute <20110613@student.hcmute.edu.vn>
2025-03-29 20:08:17 +0700

    update solution 2 for prob 1

--- Problem1_TwoSum.py
+++ Problem1_TwoSum.py
@@ -6,3 +6,15 @@ class Solution:
                 if nums[i]+nums[j]==target:
                     return [i,j]
         
+class Solution2:
+    #Dùng hash table để giảm độ phức tạp
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        #Tạo một dict với các cặp key-value là giá trị và chỉ số của phần tử trong mảng
+        for i in range(len(nums)):
+            numMap[nums[i]]=i
+        # Tính giá trị của target trừ cho từng phần tử, nếu có tồn tại trong dict và value của phần tử đó khác 
+        # chỉ số phần tử hiện tại thì trả về cặp giá trị đó
+        for i in range(len(nums)):
+            if target - nums[i] in numMap.keys() and numMap[target - nums[i]] != i:
+                return [numMap[target - nums[i]], i]
